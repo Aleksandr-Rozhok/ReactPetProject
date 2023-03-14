d2bc92e47b539e76e223f1d9e257d2c851eb00d6
Aliaksandr Razhok <aliaksandr.razhok@LeverX.local>
2023-03-14 18:02:12 +0300

    add: rewrite components, add events for inputs and for award

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -12,25 +12,21 @@ function App() {
         {
             name: "Aliaksandr Razhok",
             salary: 400,
-            increase: true,
             id: 0,
         },
         {
             name: "Kirill Haupt",
             salary: 800,
-            increase: false,
             id: 1,
         },
         {
             name: "Vasiliy Plutov",
             salary: 400,
-            increase: true,
             id: 2,
         },
         {
             name: "Hanna Kaliada",
             salary: 400,
-            increase: false,
             id: 3,
         }
     ];
--- my-app/src/components/employees-list-item/employees-list-item.js
+++ my-app/src/components/employees-list-item/employees-list-item.js
@@ -1,31 +1,58 @@
-import './employees-list-item.css'
+import { Component } from "react";
+import "./employees-list-item.css";
 
-const EmployeesListItem = ({name, salary, increase}) => {
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      award: false,
+      like: false
+    };
+  }
+
+  changeAwardState = () => {
+    this.setState(({ award }) => ({
+      award: !award
+    }));
+  };
+
+  addStarStatus = () => {
+    this.setState(({ like }) => ({
+        like: !like
+    }))
+  }
+
+  render() {
+    const { name, salary } = this.props;
+    const { award, like } = this.state;
 
     let liClasses = "list-group-item d-flex justify-content-between ";
 
-    if (increase) {
-        liClasses += "increase";
+    if (award) {
+        liClasses += "increase ";
+    }
+
+    if (like) {
+        liClasses += "like"
     }
 
     return (
-        <li className={liClasses}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+      <li className={liClasses}>
+        <span onClick={this.addStarStatus} className="list-group-item-label">{name}</span>
+        <input type="text" className="list-group-item-input" defaultValue={salary + "$"} />
+        <div className="d-flex justify-content-center align-items-center">
+          <button onClick={this.changeAwardState} type="button" className="btn-cookie btn-sm ">
+            <i className="fas fa-cookie"></i>
+          </button>
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+          <button type="button" className="btn-trash btn-sm ">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
 }
 
 export default EmployeesListItem;
